8174b198538149be7e8e96ca84ac5a6238a278c0
Cryotheus <wisedthecoder@gmail.com>
2023-03-18 20:16:36 -0600

    Wiki stuff

--- lua/pyrition/wiki/shared.lua
+++ lua/pyrition/wiki/shared.lua
@@ -6,6 +6,13 @@ local hook_prefix = "Pyrition"
 local output_path = "pyrition_2.wiki/generated"
 local link_prefix = "https://github.com/Cryotheus/pyrition_2/blob/main/lua/"
 
+--local tables
+local ignore_in_component = {
+	client = true,
+	server = true,
+	shared = true,
+}
+
 --local functions
 local function common_prefix(alpha, bravo)
 	local finish = #alpha
@@ -24,6 +31,21 @@ end
 local function _find_function_table_name(function_table)
 	--search deeper?
 	for key, value in pairs(_G) do if value == function_table then return key end end
+end
+
+local function get_nested(tree, indexing)
+	for index, key in ipairs(indexing) do
+		local branch = tree[key]
+		
+		if branch then tree = branch
+		else
+			branch = {}
+			tree[key] = branch
+			tree = branch
+		end
+	end
+
+	return tree
 end
 
 local function recursive_delete(path)
@@ -45,7 +67,6 @@ function PYRITION:WikiCollect()
 	local realm_details = {}
 	local output_path = output_path .. "/"
 
-
 	for key, value in pairs(function_table) do
 		if isstring(key) and isfunction(value) and not string.StartWith(key, hook_prefix) then
 			local info = debug.getinfo(value, "flLSu")
@@ -57,8 +78,6 @@ function PYRITION:WikiCollect()
 				if source_start and string.sub(string.sub(source, source_start, source_boiler), 8, -6) == addon_name then
 					local line = info.linedefined
 					local line_end = info.lastlinedefined
-
-					print("yep", key)
 
 					local details = {
 						Hooked = function_table[hook_prefix .. key] and true or nil,
@@ -89,16 +108,15 @@ function PYRITION:WikiGenerate()
 	local details_collection = {}
 	local documentation = {}
 	local files = file.Find(output_path .. "/_details_*.json", "DATA")
+	local structure = {}
 
 	if not files then error("No details collected!") end
 
 	for index, file_name in ipairs(files) do
-		local environment_signature = string.sub("_details_client_listen.json", 10, -6)
+		local environment_signature = string.sub(file_name, 10, -6)
 		local json = util.JSONToTable(file.Read(output_path .. "/" .. file_name, "DATA"))
 		local realm, host_method = string.Split(environment_signature, "_")
 		
-		print(index, file_name)
-
 		for key, details in pairs(json) do
 			local existing = details_collection[key]
 			local source = details.Source
@@ -125,11 +143,9 @@ function PYRITION:WikiGenerate()
 
 	local trim_prefix = #current_prefix + 1
 
-	print("prefix", current_prefix)
-
 	for key, details in pairs(details_collection) do
 		local current_line, current_line_end, current_source
-		--local trimmed_source = string.sub(details.Source, trim_prefix)
+		local environments
 		local variants = {}
 
 		for environment_signature, signed_details in pairs(details.SignedDetails) do
@@ -150,28 +166,112 @@ function PYRITION:WikiGenerate()
 					Source = source,
 				}
 
-				if documentation[trimmed_source] then table.insert(documentation[trimmed_source], information)
-				else documentation[trimmed_source] = {information} end
+				local component_docs = documentation[trimmed_source]
 
-				table.insert(environments, environment_signature)
+				if not component_docs then
+					component_docs = {}
+					documentation[trimmed_source] = component_docs
+				end
+
+				if component_docs[key] then table.insert(component_docs[key], information)
+				else component_docs[key] = {information} end
+
 				table.insert(variants, information)
 			end
+
+			table.insert(environments, environment_signature)
 		end
 	end
 
-	for trimmed_source, information in pairs(documentation) do
-		--local component_name = string.gsub(trimmed_source, "/", "")
+	for trimmed_source, methods in pairs(documentation) do
+		local component_indexing = string.Split(string.sub(trimmed_source, 1, -5), "/")
+		local component_name = string.sub(string.gsub(trimmed_source, "/", ""), 1, -5)
+		local order = {}
 		local script = file.Read(current_prefix .. trimmed_source, "LUA")
-		local script_lines = script and string.Split(script, "\n")
+		--local script_lines = script and string.Split(script, "\n")
 
-		print(script_lines and #script_lines or "invalid")
-		print(trimmed_source, string.GetPathFromFilename(trimmed_source))
-		print("line:", script_lines and script_lines[information.Line])
-		PrintTable(information)
+		local last_component = component_indexing[#component_indexing]
 
-		break
-		--file.Write(file_path, file.Read(trimmed_source, "GAME"))
+		--remove client, server, and shared endings from component names
+		if ignore_in_component[last_component] then
+			component_name = string.sub(component_name, 1, -1 - #last_component)
+
+			table.remove(component_indexing)
+		end
+
+		local tree = get_nested(structure, component_indexing)
+
+		for key in pairs(methods) do table.insert(order, key) end
+
+		for order_index, key in ipairs(order) do
+			local common_text = common_prefix(component_name, string.lower(key))
+			local method_name = string.sub(key, #common_text + 1)
+			local method_parent = string.sub(key, 1, #common_text)
+
+			--print("component_indexing", component_name, unpack(component_indexing))
+			local file_path = table.concat(component_indexing, "/")
+			local meta_data = {
+				FilePath = file_path .. ".txt",
+				ParentComponenet = method_parent,
+			}
+
+			meta_data.Path = string.sub(file_path, 1, -1 - #component_indexing[#component_indexing])
+			tree[0] = meta_data
+
+			for variant_index, details in ipairs(methods[key]) do
+				--it's hard to scrap
+				local data = {
+					Name = method_name,
+					Line = details.Line,
+					LineEnd = details.LineEnd,
+					Link = details.Link,
+				}
+
+				for index, environment_signature in ipairs(details.Environments) do
+					if string.StartWith(environment_signature, "client_") then data.Client = true
+					elseif string.StartWith(environment_signature, "server_") then data.Server = true end
+				end
+
+				table.insert(tree, data)
+			end
+		end
 	end
+	
+	local function write_files(tree)
+		local meta_data = tree[0]
+		--tree[0] = nil
+
+		if meta_data then
+			local file_path = meta_data.FilePath
+			local parent_component = meta_data.ParentComponenet
+			local path = meta_data.Path
+
+			local mark_down = "# " .. parent_component .. "\n\n"
+
+			--print("parent_component", parent_component)
+
+			for index, method_details in ipairs(tree) do
+				--tree[index] = nil
+				local icon
+
+				if method_details.Client and method_details.Server then icon = "shared"
+				elseif method_details.Client then icon = "client"
+				elseif method_details.Server then icon = "server" end 
+
+				mark_down = mark_down .. "## []" .. method_details.Name .. "](" .. method_details.Link .. ")\n\n"
+			end
+
+			file.CreateDir(output_path .. "/" .. path)
+			file.Write(output_path .. "/" .. file_path, mark_down)
+		end
+
+		for key, value in pairs(tree) do
+			if not isnumber(key) then write_files(value) end
+		end
+	end
+
+	--PrintTable(structure)
+	write_files(structure)
 end
 
 --commands
@@ -194,49 +294,27 @@ concommand.Add(command_prefix .. "pyrition_wiki_purge", function(ply)
 end)
 
 --[[
-	ConsoleCommandArgumentGetSettingMacro:
-		activelines:
-			34	=	true
-			36	=	true
-		currentline	=	-1
-		func	=	function: 0x01117e197918
-		isvararg	=	false
-		lastlinedefined	=	37
-		linedefined	=	33
-		nparams	=	2
-		nups	=	0
-		short_src	=	addons/pyrition/lua/pyrition/console/command_argument.lua
-		source	=	@addons/pyrition/lua/pyrition/console/command_argument.lua
-		what	=	Lua
-	ConsoleCommandArgumentParse:
-		activelines:
-			40	=	true
-			41	=	true
-			43	=	true
-			44	=	true
-			45	=	true
-			46	=	true
-			48	=	true
-			50	=	true
-			52	=	true
-			54	=	true
-			55	=	true
-			56	=	true
-			58	=	true
-			60	=	true
-			61	=	true
-			64	=	true
-			65	=	true
-			67	=	true
-			69	=	true
-		currentline	=	-1
-		func	=	function: 0x011184315a58
-		isvararg	=	false
-		lastlinedefined	=	70
-		linedefined	=	39
-		nparams	=	2
-		nups	=	1
-		short_src	=	addons/pyrition/lua/pyrition/console/command_argument.lua
-		source	=	@addons/pyrition/lua/pyrition/console/command_argument.lua
-		what	=	Lua
+	console:
+		1:
+			Client	=	true
+			Line	=	20
+			LineEnd	=	43
+			Link	=	https://github.com/Cryotheus/pyrition_2/blob/main/lua/pyrition/console/shared.lua#L20-L43
+			Name	=	ParseArguments
+			Server	=	true
+		2:
+			Client	=	true
+			Line	=	45
+			LineEnd	=	72
+			Link	=	https://github.com/Cryotheus/pyrition_2/blob/main/lua/pyrition/console/shared.lua#L45-L72
+			Name	=	ParseString
+			Server	=	true
+		command:
+			1:
+				Client	=	true
+				Line	=	27
+				LineEnd	=	42
+				Link	=	https://github.com/Cryotheus/pyrition_2/blob/main/lua/pyrition/console/command.lua#L27-L42
+				Name	=	SignatureTreeSet
+				Server	=	true
 ]]
