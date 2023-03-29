5a864923e698357d960edd02b50ebdb6087d44d8
Cryotheus <wisedthecoder@gmail.com>
2023-03-29 15:28:24 -0600

    GetPlayerClass* Player meta methods

--- lua/pyrition/player/meta/shared.lua
+++ lua/pyrition/player/meta/shared.lua
@@ -1,8 +1,20 @@
 --locals
+local baseclass_Get = baseclass.Get
+local player_manager_GetPlayerClass = player_manager.GetPlayerClass
 local player_meta = FindMetaTable("Player")
 local world = game.GetWorld()
 
 --player meta functions
+function player_meta:GetPlayerClass() return player_manager_GetPlayerClass(self) end
+
+function player_meta:GetPlayerClassNWField(field, field_type)
+	local class_table = baseclass_Get(player_manager_GetPlayerClass(self))
+	local get_network_field = class_table.GetNetworkField
+
+	return get_network_field and get_network_field(class_table, field, field_type)
+end
+
+function player_meta:GetPlayerClassTable() return baseclass_Get(player_manager_GetPlayerClass(self)) end
 function player_meta:IsPlayerOrWorld() return self:IsPlayer() or self == world end
 
 --hooks
