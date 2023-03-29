87613986721e220f9a43d755d9e95b44482b4bf2
Cryotheus <wisedthecoder@gmail.com>
2023-03-29 15:32:42 -0600

    Bug fix

--- lua/pyrition/player/meta/shared.lua
+++ lua/pyrition/player/meta/shared.lua
@@ -10,8 +10,13 @@ function player_meta:GetPlayerClass() return player_manager_GetPlayerClass(self)
 function player_meta:GetPlayerClassNWField(field, field_type)
 	local class_table = baseclass_Get(player_manager_GetPlayerClass(self))
 	local get_network_field = class_table.GetNetworkField
+	local reset_ply = class_table.Player
+	class_table.Player = self
 
-	return get_network_field and get_network_field(class_table, field, field_type)
+	local returns = get_network_field and get_network_field(class_table, field, field_type)
+	class_table.Player = reset_ply
+
+	return returns
 end
 
 function player_meta:GetPlayerClassTable() return baseclass_Get(player_manager_GetPlayerClass(self)) end
