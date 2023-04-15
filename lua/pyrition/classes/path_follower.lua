908cc863e4e8d49e6215b34f8e5be20945ba6699
Cryotheus <wisedthecoder@gmail.com>
2023-04-15 02:55:32 -0600

    Fixed PathFollower:GetDeduplicatedSegments()

--- lua/pyrition/classes/path_follower.lua
+++ lua/pyrition/classes/path_follower.lua
@@ -6,9 +6,17 @@ function path_follower_meta:GetDeduplicatedSegments()
 	---Same as PathFollower:GetAllSegments, but removes any segments that are in the same area as the previous segment.
 	local index = 2
 	local segments = self:GetAllSegments()
+
+	table.remove(segments, 1)
+
 	local segments_count = #segments
 
-	while index < segments_count do
+	if segments_count > 1 and segments[segments_count].area == segments[segments_count - 1].area then
+		segments_count = segments_count - 1
+		segments[segments_count] = nil
+	end
+
+	while index <= segments_count do
 		local last_segment = segments[index - 1]
 		local segment = segments[index]
 
